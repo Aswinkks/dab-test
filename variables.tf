16391a8b76f9b7a6afe0bb0e6ccf5ed164cd055e
Aswin <aswin@Aswins-MacBook-Air.local>
2024-05-30 00:56:56 +0530

    wsa

--- variables.tf
+++ variables.tf
@@ -8,11 +8,9 @@ variable "location" {
 variable "project_id" {
   description = "The project ID to deploy to"
   type        = string
-  default = "itsme-1234"
 }
 
 variable "bucket_name" {
   description = "The name of the bucket to create"
   type        = string
-  default = "idkslnfae"
 }
